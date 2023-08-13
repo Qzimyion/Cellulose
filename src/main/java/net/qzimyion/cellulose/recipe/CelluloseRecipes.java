bde6da6eeb0e88d2dd8b07da03ae3ef382b9ef25
Qzimyion <samyakchauhan676@gmail.com>
2023-08-13 21:41:29 +0530

    update

--- src/main/java/net/qzimyion/cellulose/recipe/CelluloseRecipes.java
+++ src/main/java/net/qzimyion/cellulose/recipe/CelluloseRecipes.java
@@ -1,6 +1,5 @@
 package net.qzimyion.cellulose.recipe;
 
-import net.minecraft.recipe.RecipeType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -10,7 +9,10 @@ public class CelluloseRecipes {
 
     public static void registerRecipes() {
         Cellulose.LOGGER.info("Registering Recipe Types for " + Cellulose.MOD_ID);
+
         Registry.register(Registries.RECIPE_SERIALIZER, new Identifier(Cellulose.MOD_ID, SawmillingRecipe.Serializer.ID),
                 SawmillingRecipe.Serializer.INSTANCE);
+        Registry.register(Registries.RECIPE_TYPE, new Identifier(Cellulose.MOD_ID, SawmillingRecipe.Type.ID),
+                SawmillingRecipe.Type.INSTANCE);
     }
 }
