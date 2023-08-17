c83e6281fb000e77484caf21a0a08ec40b2f4a66
Qzimyion <samyakchauhan676@gmail.com>
2023-08-17 16:50:06 +0530

    Updated PickaxeMixin.java

--- src/main/java/net/qzimyion/cellulose/mixin/PickaxeMixin.java
+++ src/main/java/net/qzimyion/cellulose/mixin/PickaxeMixin.java
@@ -2,65 +2,70 @@ package net.qzimyion.cellulose.mixin;
 
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
+import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.PillarBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.*;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
 import net.qzimyion.cellulose.registry.CelluloseBlocks;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 import java.util.Map;
+import java.util.Optional;
 
-@Debug(export = true)
+
 @Mixin(PickaxeItem.class)
-public class PickaxeMixin extends Item {
-    @Unique
-    private static final Map<Block, BlockState> CHIPPED_BLOCKS = Maps.newHashMap(new ImmutableMap.Builder<Block, BlockState>()
-            .put(Blocks.OAK_WOOD, CelluloseBlocks.CHIPPED_OAK_WOOD.getDefaultState()).put(Blocks.OAK_LOG, CelluloseBlocks.CHIPPED_OAK.getDefaultState())
-            .build());
-
-    public PickaxeMixin(Settings settings) {
-        super(settings);
+public class PickaxeMixin extends MiningToolItem {
+    public PickaxeMixin(float attackDamage, float attackSpeed, ToolMaterial material, TagKey<Block> effectiveBlocks, Settings settings) {
+        super(attackDamage, attackSpeed, material, effectiveBlocks, settings);
     }
 
+
     @Override
-    public ActionResult useOnBlock(ItemUsageContext context) {
+    public ActionResult useOnBlock(ItemUsageContext context){
         BlockPos blockPos = context.getBlockPos();
         World world = context.getWorld();
         BlockState blockState = world.getBlockState(blockPos);
-        if (context.getSide() != Direction.DOWN) {
-            PlayerEntity playerEntity = context.getPlayer();
-            BlockState blockState2 = CHIPPED_BLOCKS.get(blockState.getBlock());
-            BlockState blockState3 = null;
-            if (blockState2 != null && world.getBlockState(blockPos.up()).isAir()) {
-                world.playSound(playerEntity, blockPos, SoundEvents.ENTITY_PAINTING_BREAK, SoundCategory.BLOCKS, 1.0f, 1.0f);
-                blockState3 = blockState2;
-            }
-            if (blockState3 != null) {
-                if (!world.isClient) {
-                    world.setBlockState(blockPos, blockState3, Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
-                    world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, blockState3));
-                    if (playerEntity != null) {
-                        context.getStack().damage(1, playerEntity, p -> p.sendToolBreakStatus(context.getHand()));
-                    }
-                }
-                return ActionResult.success(world.isClient);
-            }
-            return ActionResult.PASS;
+        Optional<BlockState> optional = this.getChippedState(blockState);
+        Optional<BlockState> optional1 = Optional.empty();
+        PlayerEntity playerEntity = context.getPlayer();
+        ItemStack itemStack = context.getStack();
+        if (optional.isPresent()){
+            world.playSound(playerEntity, blockPos, SoundEvents.ENTITY_PAINTING_BREAK, SoundCategory.BLOCKS, 1.0f, 1.0f);
+            optional1 = optional;
         }
-        return ActionResult.PASS;
+        if (optional1.isPresent()) {
+            if (playerEntity instanceof ServerPlayerEntity) {
+                Criteria.ITEM_USED_ON_BLOCK.trigger((ServerPlayerEntity) playerEntity, blockPos, itemStack);
+            }
+            world.setBlockState(blockPos, optional1.get(), Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
+            world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, optional1.get()));
+            if (playerEntity != null) {
+                itemStack.damage(1, playerEntity, player -> player.sendToolBreakStatus(context.getHand()));
+            }
+            return ActionResult.success(world.isClient);
+        }
+        return super.useOnBlock(context);
     }
+
+    @Unique
+    private Optional<BlockState> getChippedState(BlockState state) {
+        return Optional.ofNullable(CHIPPED_BLOCKS.get(state.getBlock())).map(block -> block.getDefaultState().with(PillarBlock.AXIS, state.get(PillarBlock.AXIS)));
+    }
+
+    @Unique
+    private static final Map<Block, Block> CHIPPED_BLOCKS = new ImmutableMap.Builder<Block, Block>()
+            .put(Blocks.OAK_WOOD, CelluloseBlocks.CHIPPED_OAK_WOOD).put(Blocks.OAK_LOG, CelluloseBlocks.CHIPPED_OAK)
+            .build();
 }
