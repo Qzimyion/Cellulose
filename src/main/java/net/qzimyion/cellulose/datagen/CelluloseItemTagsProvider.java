f187165ee89fd40737555f7d1d559555ed31d6dc
Qzimyion <samyakchauhan676@gmail.com>
2023-08-20 14:16:58 +0530

    Added CelluloseItemTagsProvider.java

---
+++ src/main/java/net/qzimyion/cellulose/datagen/CelluloseItemTagsProvider.java
@@ -0,0 +1,24 @@
+package net.qzimyion.cellulose.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+import java.util.concurrent.CompletableFuture;
+
+import static net.qzimyion.cellulose.items.CelluloseItems.*;
+
+public class CelluloseItemTagsProvider extends FabricTagProvider.ItemTagProvider {
+    public CelluloseItemTagsProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void configure(RegistryWrapper.WrapperLookup arg) {
+
+    }
+}
