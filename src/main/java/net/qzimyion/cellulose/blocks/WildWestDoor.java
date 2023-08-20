0fbfc10f36846081d1ccddb0d1da3d75da297632
Qzimyion <samyakchauhan676@gmail.com>
2023-08-20 17:59:29 +0530

    Added

---
+++ src/main/java/net/qzimyion/cellulose/blocks/WildWestDoor.java
@@ -0,0 +1,39 @@
+package net.qzimyion.cellulose.blocks;
+
+import net.minecraft.block.*;
+import net.minecraft.block.enums.DoorHinge;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+
+public class WildWestDoor extends DoorBlock {
+    public WildWestDoor(Settings settings, BlockSetType blockSetType) {
+        super(settings, blockSetType);
+    }
+
+    protected static final VoxelShape NORTH_SHAPE = Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 16.0, 3.0);
+    protected static final VoxelShape SOUTH_SHAPE = Block.createCuboidShape(0.0, 0.0, 13.0, 16.0, 16.0, 16.0);
+    protected static final VoxelShape EAST_SHAPE = Block.createCuboidShape(13.0, 0.0, 0.0, 16.0, 16.0, 16.0);
+    protected static final VoxelShape WEST_SHAPE = Block.createCuboidShape(0.0, 0.0, 0.0, 3.0, 16.0, 16.0);
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        Direction direction = state.get(FACING);
+        boolean bl = state.get(OPEN) == false;
+        boolean bl2 = state.get(HINGE) == DoorHinge.RIGHT;
+        switch (direction) {
+            default: {
+                return bl ? WEST_SHAPE : (bl2 ? SOUTH_SHAPE : NORTH_SHAPE);
+            }
+            case SOUTH: {
+                return bl ? NORTH_SHAPE : (bl2 ? WEST_SHAPE : EAST_SHAPE);
+            }
+            case WEST: {
+                return bl ? EAST_SHAPE : (bl2 ? NORTH_SHAPE : SOUTH_SHAPE);
+            }
+            case NORTH:
+        }
+        return bl ? SOUTH_SHAPE : (bl2 ? EAST_SHAPE : WEST_SHAPE);
+    }
+}
