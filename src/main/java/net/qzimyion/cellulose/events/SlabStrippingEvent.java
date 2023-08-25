37f55097459f169e9dc82e2507859eeafbaab063
Qzimyion <samyakchauhan676@gmail.com>
2023-08-25 13:39:18 +0530

    Added Test slab stripping code

---
+++ src/main/java/net/qzimyion/cellulose/events/SlabStrippingEvent.java
@@ -0,0 +1,34 @@
+package net.qzimyion.cellulose.events;
+
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.event.GameEvent;
+
+public class SlabStrippingEvent {
+
+    public static void SlabStripping(Block input, Block output) {
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+            BlockPos pos = hitResult.getBlockPos();
+            BlockState state = world.getBlockState(pos);
+            ItemStack stack = player.getStackInHand(hand);
+            if (input != null && output != null) {
+                if (state.isOf(input) && stack.isIn(ItemTags.AXES)) {
+                    stack.damage(1, player, (p) -> p.sendToolBreakStatus(hand));
+                    player.swingHand(hand);
+                    world.setBlockState(pos, output.getStateWithProperties(state));
+                    world.emitGameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Emitter.of(player, output.getStateWithProperties(state)));
+                    world.playSound(player, pos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    return ActionResult.SUCCESS;
+                }
+            }
+            return ActionResult.PASS;
+        });
+    }
+}
