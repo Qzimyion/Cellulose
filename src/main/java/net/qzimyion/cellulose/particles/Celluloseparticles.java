643b3c40f28ece45bd7ddbfeb80d5b1200f40d6b
Qzimyion <samyakchauhan676@gmail.com>
2023-08-26 12:07:11 +0530

    Added experimental stuff for particles and signs

---
+++ src/main/java/net/qzimyion/cellulose/particles/Celluloseparticles.java
@@ -0,0 +1,16 @@
+package net.qzimyion.cellulose.particles;
+
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.particle.DefaultParticleType;
+import net.qzimyion.cellulose.Cellulose;
+
+public class Celluloseparticles {
+
+    public static final DefaultParticleType OAK_FALLING_LEAF = FabricParticleTypes.simple();
+
+    public static void registerParticles(){
+
+
+        Cellulose.LOGGER.info("Registering particles for" + Cellulose.MOD_ID);
+    }
+}
