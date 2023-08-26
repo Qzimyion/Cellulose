19a328a499eca05c62ccc63a31b26b21982eb1fa
Qzimyion <samyakchauhan676@gmail.com>
2023-08-26 12:08:52 +0530

    Made vertical wood and wooden mosaic assets Datagenable

---
+++ src/main/java/net/qzimyion/cellulose/entity/block_entity/Oak_bark_hive_block_entity.java
@@ -0,0 +1,4 @@
+package net.qzimyion.cellulose.entity.block_entity;
+
+public class Oak_bark_hive_block_entity {
+}
---
+++ src/main/java/net/qzimyion/cellulose/mixin/OakLeavesMixin.java
@@ -0,0 +1,7 @@
+package net.qzimyion.cellulose.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(OakLeavesMixin.class)
+public class OakLeavesMixin {
+}
