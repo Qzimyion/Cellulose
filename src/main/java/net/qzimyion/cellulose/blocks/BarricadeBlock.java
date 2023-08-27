1ecc05d701f7a5fe286d169263f16c4be8757127
Qzimyion <samyakchauhan676@gmail.com>
2023-08-27 20:18:34 +0530

    Added Jsons and Textures for Cactus signs and hanging signs

---
+++ src/main/java/net/qzimyion/cellulose/blocks/BarricadeBlock.java
@@ -0,0 +1,10 @@
+package net.qzimyion.cellulose.blocks;
+
+import net.minecraft.block.PaneBlock;
+
+public class BarricadeBlock extends PaneBlock {
+
+    public BarricadeBlock(Settings settings) {
+        super(settings);
+    }
+}
