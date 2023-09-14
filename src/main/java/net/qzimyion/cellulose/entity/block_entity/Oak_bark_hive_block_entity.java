ee039a90f3d148fdea255aed7a11610a0337c647
Qzimyion <samyakchauhan676@gmail.com>
2023-09-15 02:10:49 +0530

    Removed

--- src/main/java/net/qzimyion/cellulose/entity/block_entity/Oak_bark_hive_block_entity.java
+++
@@ -1,4 +0,0 @@
-package net.qzimyion.cellulose.entity.block_entity;
-
-public class Oak_bark_hive_block_entity {
-}
--- src/main/java/net/qzimyion/cellulose/mixin/OakLeavesMixin.java
+++
@@ -1,7 +0,0 @@
-package net.qzimyion.cellulose.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(OakLeavesMixin.class)
-public class OakLeavesMixin {
-}
