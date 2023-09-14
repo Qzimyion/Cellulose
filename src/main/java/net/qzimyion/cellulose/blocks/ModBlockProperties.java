bd82808a4910c7e36b71665ed4c43e0b0ee34dc9
Qzimyion <samyakchauhan676@gmail.com>
2023-09-15 02:11:56 +0530

    Added ModBlockProperties file

---
+++ src/main/java/net/qzimyion/cellulose/blocks/ModBlockProperties.java
@@ -0,0 +1,15 @@
+package net.qzimyion.cellulose.blocks;
+
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.IntProperty;
+
+public class ModBlockProperties {
+
+    public static final IntProperty FRUIT_AMOUNT = IntProperty.of("fruit_amount", 1, 4);
+    public static final IntProperty LEAF_LAYERS = IntProperty.of("leaf_layers", 1, 16);
+    public static final BooleanProperty LOTUS = BooleanProperty.of("lotus");
+
+    //Triple door Properties
+
+
+}
