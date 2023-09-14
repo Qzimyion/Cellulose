b98db0448104410f443637f74250597a606fb03d
Qzimyion <samyakchauhan676@gmail.com>
2023-09-15 02:21:45 +0530

    Gipple

--- src/main/java/net/qzimyion/cellulose/mixin/ExampleMixin.java
+++ src/main/java/net/qzimyion/cellulose/mixin/ExampleMixin.java
@@ -1,11 +1,13 @@
 package net.qzimyion.cellulose.mixin;
 
 import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@Debug(export = true)
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
