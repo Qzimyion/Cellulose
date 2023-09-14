6d9265e2dad6b20d23ddf88d5427c70ab97e865c
Qzimyion <samyakchauhan676@gmail.com>
2023-09-15 02:01:52 +0530

    Added Acorn stuff

---
+++ src/main/java/net/qzimyion/cellulose/items/custom_items/popacorn_bowls/PopacornBowlHalfItem.java
@@ -0,0 +1,23 @@
+package net.qzimyion.cellulose.items.custom_items.popacorn_bowls;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.qzimyion.cellulose.items.CelluloseItems;
+
+public class PopacornBowlHalfItem extends Item {
+    public PopacornBowlHalfItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        ItemStack itemStack = super.finishUsing(stack, world, user);
+        if (user instanceof PlayerEntity && ((PlayerEntity)user).getAbilities().creativeMode) {
+            return itemStack;
+        }
+        return new ItemStack(CelluloseItems.POPACORN_BOWL_QUATER);
+    }
+}
---
+++ src/main/java/net/qzimyion/cellulose/items/custom_items/popacorn_bowls/PopacornBowlQuaterItem.java
@@ -0,0 +1,24 @@
+package net.qzimyion.cellulose.items.custom_items.popacorn_bowls;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.world.World;
+import net.qzimyion.cellulose.items.CelluloseItems;
+
+public class PopacornBowlQuaterItem extends Item {
+    public PopacornBowlQuaterItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        ItemStack itemStack = super.finishUsing(stack, world, user);
+        if (user instanceof PlayerEntity && ((PlayerEntity)user).getAbilities().creativeMode) {
+            return itemStack;
+        }
+        return new ItemStack(Items.BOWL);
+    }
+}
