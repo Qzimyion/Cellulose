e1b48e8bd48f68802d722da4be83f2ac511a2c9f
Qzimyion <samyakchauhan676@gmail.com>
2023-09-22 23:52:08 +0530

    Added values for chipped stripped logs and wood

--- src/main/java/net/qzimyion/cellulose/blocks/CelluloseStrippableBlock.java
+++ src/main/java/net/qzimyion/cellulose/blocks/CelluloseStrippableBlock.java
@@ -34,6 +34,7 @@ public class CelluloseStrippableBlock
         StrippableBlockRegistry.register(CACTUS_CROWN, STRIPPED_CACTUS_CROWN);
         StrippableBlockRegistry.register(STRIPPED_CACTUS, ENGRAVED_CACTUS);
         StrippableBlockRegistry.register(STRIPPED_CACTUS_CROWN, ENGRAVED_CACTUS_CROWN);
+        StrippableBlockRegistry.register(CHIPPED_CACTUS_CROWN, STRIPPED_CHIPPED_CACTUS_CROWN);
         //Azalea stuff
         StrippableBlockRegistry.register(AZALEA_LOG, STRIPPED_AZALEA);
         StrippableBlockRegistry.register(AZALEA_WOOD, STRIPPED_AZALEA_WOOD);
@@ -43,12 +44,39 @@ public class CelluloseStrippableBlock
         StrippableBlockRegistry.register(BLOOMING_AZALEA_WOOD, BLOOMING_STRIPPED_AZALEA_WOOD);
         StrippableBlockRegistry.register(BLOOMING_STRIPPED_AZALEA, ENGRAVED_BLOOMING_AZALEA);
         StrippableBlockRegistry.register(BLOOMING_STRIPPED_AZALEA_WOOD, ENGRAVED_BLOOMING_AZALEA_WOOD);
-        //Overworld Mushroom stuff
+        StrippableBlockRegistry.register(CHIPPED_AZALEA_WOOD, STRIPPED_CHIPPED_AZALEA_WOOD);
+        //Chipped Stripped
+        StrippableBlockRegistry.register(CHIPPED_OAK, STRIPPED_CHIPPED_OAK);
+        StrippableBlockRegistry.register(CHIPPED_BIRCH, STRIPPED_CHIPPED_BIRCH);
+        StrippableBlockRegistry.register(CHIPPED_SPRUCE, STRIPPED_CHIPPED_SPRUCE);
+        StrippableBlockRegistry.register(CHIPPED_DARK_OAK, STRIPPED_CHIPPED_DARK_OAK);
+        StrippableBlockRegistry.register(CHIPPED_JUNGLE, STRIPPED_CHIPPED_JUNGLE);
+        StrippableBlockRegistry.register(CHIPPED_ACACIA, STRIPPED_CHIPPED_ACACIA);
+        StrippableBlockRegistry.register(CHIPPED_CRIMSON, STRIPPED_CHIPPED_CRIMSON);
+        StrippableBlockRegistry.register(CHIPPED_WARPED, STRIPPED_CHIPPED_WARPED);
+        StrippableBlockRegistry.register(CHIPPED_MANGROVE, STRIPPED_CHIPPED_MANGROVE);
+        StrippableBlockRegistry.register(CHIPPED_CHERRY, STRIPPED_CHIPPED_CHERRY);
+        StrippableBlockRegistry.register(CHIPPED_CACTUS, STRIPPED_CHIPPED_CACTUS);
+        StrippableBlockRegistry.register(CHIPPED_AZALEA, STRIPPED_CHIPPED_AZALEA);
+        StrippableBlockRegistry.register(CHIPPED_OAK_WOOD, STRIPPED_CHIPPED_OAK_WOOD);
+        StrippableBlockRegistry.register(CHIPPED_BIRCH_WOOD, STRIPPED_CHIPPED_BIRCH_WOOD);
+        StrippableBlockRegistry.register(CHIPPED_SPRUCE_WOOD, STRIPPED_CHIPPED_SPRUCE_WOOD);
+        StrippableBlockRegistry.register(CHIPPED_DARK_OAK_WOOD, STRIPPED_CHIPPED_DARK_OAK_WOOD);
+        StrippableBlockRegistry.register(CHIPPED_JUNGLE_WOOD, STRIPPED_CHIPPED_JUNGLE_WOOD);
+        StrippableBlockRegistry.register(CHIPPED_ACACIA_WOOD, STRIPPED_CHIPPED_ACACIA_WOOD);
+        StrippableBlockRegistry.register(CHIPPED_CRIMSON_HYPHAE, STRIPPED_CHIPPED_CRIMSON_HYPHAE);
+        StrippableBlockRegistry.register(CHIPPED_WARPED_HYPHAE, STRIPPED_CHIPPED_WARPED_HYPHAE);
+        StrippableBlockRegistry.register(CHIPPED_MANGROVE_WOOD, STRIPPED_CHIPPED_MANGROVE_WOOD);
+        StrippableBlockRegistry.register(CHIPPED_CHERRY_WOOD, STRIPPED_CHIPPED_CHERRY_WOOD);
+
+        ///Overworld Mushroom stuff
         /*Red*/
 
         /*Brown*/
-        //Azalea Wood
 
-        //Gay™ wood
+
+        ///Petrified Wood
+
+        ///Gay™ wood
     }
 }
