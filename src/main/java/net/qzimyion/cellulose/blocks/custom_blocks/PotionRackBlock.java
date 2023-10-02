94cf0236fcbe969eeed50a5723e8b827e478d7b4
Qzimyion <samyakchauhan676@gmail.com>
2023-10-02 18:11:40 +0530

    Added potion racks

---
+++ src/main/java/net/qzimyion/cellulose/blocks/custom_blocks/PotionRackBlock.java
@@ -0,0 +1,322 @@
+package net.qzimyion.cellulose.blocks.custom_blocks;
+
+import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.*;
+import net.minecraft.world.event.GameEvent;
+import net.qzimyion.cellulose.entity.BlockEntity.PotionRackBlockEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.qzimyion.cellulose.util.CelluloseTags;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Optional;
+
+@SuppressWarnings("deprecation, unused")
+public class PotionRackBlock extends BlockWithEntity {
+    private static final int MAX_POTION_COUNT = 6;
+    private static final int MAX_GLASS_BOTTLE_COUNT = 6;
+    private static final int MAX_WATER_BOTTLE_COUNT = 6;
+    private static final int MAX_HONEY_BOTTLE_COUNT = 6;
+    public static final int POTION_HEIGHT = 3;
+    public static final int GLASS_BOTTLE_HEIGHT = 3;
+    public static final int WATER_BOTTLE_HEIGHT = 3;
+    public static final int HONEY_BOTTLE_HEIGHT = 3;
+    public static final List<BooleanProperty> SLOT_OCCUPIED_PROPERTIES = List.of(Properties.SLOT_0_OCCUPIED, Properties.SLOT_1_OCCUPIED, Properties.SLOT_2_OCCUPIED, Properties.SLOT_3_OCCUPIED, Properties.SLOT_4_OCCUPIED, Properties.SLOT_5_OCCUPIED);
+
+    public PotionRackBlock(Settings settings) {
+        super(settings);
+        BlockState blockState = this.stateManager.getDefaultState().with(HorizontalFacingBlock.FACING, Direction.NORTH);
+        for (BooleanProperty booleanProperty : SLOT_OCCUPIED_PROPERTIES) {
+            blockState = blockState.with(booleanProperty, false);
+        }
+        this.setDefaultState(blockState);
+    }
+
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit){
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (!(blockEntity instanceof PotionRackBlockEntity potionRackBlockEntity)){
+            return ActionResult.PASS;
+        }
+        Optional<Vec2f> optional = PotionRackBlock.getHitPos(hit, state.get(HorizontalFacingBlock.FACING));
+        if (optional.isEmpty()) {
+            return ActionResult.PASS;
+        }
+        int i = PotionRackBlock.getSlotForHitPos(optional.get());
+        if (state.get(SLOT_OCCUPIED_PROPERTIES.get(i))){
+            PotionRackBlock.tryRemovingPotions(world, pos, player, potionRackBlockEntity, i);
+            return ActionResult.success(world.isClient);
+        }
+        ItemStack itemStack = player.getStackInHand(hand);
+        //Potions
+        if ((itemStack.isIn(CelluloseTags.Items.POTION_RACK_POTIONS))){
+            PotionRackBlock.tryAddingPotions(world, pos, player, potionRackBlockEntity, itemStack, i);
+            return ActionResult.success(world.isClient);
+        }
+        //Glass bottles
+        if (state.get(SLOT_OCCUPIED_PROPERTIES.get(i))){
+            PotionRackBlock.tryRemovingGlassBottles(world, pos, player, potionRackBlockEntity, i);
+            return ActionResult.success(world.isClient);
+        }
+        ItemStack itemStack1 = player.getStackInHand(hand);
+        if ((itemStack.isIn(CelluloseTags.Items.POTION_RACK_POTIONS))){
+            PotionRackBlock.tryAddingGlassBottles(world, pos, player, potionRackBlockEntity, itemStack1, i);
+            return ActionResult.success(world.isClient);
+        }
+        //Water Bottles
+        if (state.get(SLOT_OCCUPIED_PROPERTIES.get(i))){
+            PotionRackBlock.tryRemovingWaterBottles(world, pos, player, potionRackBlockEntity, i);
+            return ActionResult.success(world.isClient);
+        }
+        ItemStack itemStack2 = player.getStackInHand(hand);
+        if ((itemStack.isIn(CelluloseTags.Items.POTION_RACK_POTIONS))){
+            PotionRackBlock.tryAddingWaterBottles(world, pos, player, potionRackBlockEntity, itemStack2, i);
+            return ActionResult.success(world.isClient);
+        }
+        //Honey Bottles
+        if (state.get(SLOT_OCCUPIED_PROPERTIES.get(i))){
+            PotionRackBlock.tryRemovingHoneyBottles(world, pos, player, potionRackBlockEntity, i);
+            return ActionResult.success(world.isClient);
+        }
+        ItemStack itemStack3 = player.getStackInHand(hand);
+        if ((itemStack.isIn(CelluloseTags.Items.POTION_RACK_POTIONS))){
+            PotionRackBlock.tryAddingHoneyBottles(world, pos, player, potionRackBlockEntity, itemStack3, i);
+            return ActionResult.success(world.isClient);
+        }
+
+        return ActionResult.CONSUME;
+    }
+
+    public static Optional<Vec2f> getHitPos(BlockHitResult hit, Direction facing) {
+        Direction direction = hit.getSide();
+        if (facing != direction) {
+            return Optional.empty();
+        }
+        BlockPos blockPos = hit.getBlockPos().offset(direction);
+        Vec3d vec3d = hit.getPos().subtract(blockPos.getX(), blockPos.getY(), blockPos.getZ());
+        double d = vec3d.getX();
+        double e = vec3d.getY();
+        double f = vec3d.getZ();
+        return switch (direction) {
+            case NORTH -> Optional.of(new Vec2f((float)(1.0 - d), (float)e));
+            case SOUTH -> Optional.of(new Vec2f((float)d, (float)e));
+            case WEST -> Optional.of(new Vec2f((float)f, (float)e));
+            case EAST -> Optional.of(new Vec2f((float)(1.0 - f), (float)e));
+            case DOWN,UP -> Optional.empty();
+        };
+    }
+
+    public static int getSlotForHitPos(Vec2f hitPos) {
+        int i = hitPos.y >= 0.5f ? 0 : 1;
+        int j = PotionRackBlock.getColumn(hitPos.x);
+        return j + i * 3;
+    }
+
+    public static int getColumn(float x) {
+        float f = 0.0625f;
+        float g = 0.375f;
+        if (x < 0.375f) {
+            return 0;
+        }
+        float h = 0.6875f;
+        if (x < 0.6875f) {
+            return 1;
+        }
+        return 2;
+    }
+
+    ///For potions
+    public static void tryAddingPotions(World world, BlockPos pos, PlayerEntity player, PotionRackBlockEntity blockEntity, ItemStack stack, int slot){
+        if (world.isClient) {
+            return;
+        }
+        player.incrementStat(Stats.USED.getOrCreateStat(stack.getItem()));
+        SoundEvent soundEvent = stack.isOf(Items.ENCHANTED_BOOK) ? SoundEvents.BLOCK_CHISELED_BOOKSHELF_INSERT_ENCHANTED : SoundEvents.BLOCK_CHISELED_BOOKSHELF_INSERT;
+        blockEntity.setStack(slot, stack.split(1));
+        world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        if (player.isCreative()) {
+            stack.increment(1);
+        }
+        world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+    }
+
+    public static void tryRemovingPotions(World world, BlockPos pos, PlayerEntity player, PotionRackBlockEntity blockEntity, int slot) {
+        if (world.isClient) {
+            return;
+        }
+        ItemStack itemStack = blockEntity.removeStack(slot, 1);
+        SoundEvent soundEvent = itemStack.isOf(Items.ENCHANTED_BOOK) ? SoundEvents.BLOCK_CHISELED_BOOKSHELF_PICKUP_ENCHANTED : SoundEvents.BLOCK_CHISELED_BOOKSHELF_PICKUP;
+        world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        if (!player.getInventory().insertStack(itemStack)) {
+            player.dropItem(itemStack, false);
+        }
+        world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+    }
+    ///For empty Bottles
+    public static void tryAddingGlassBottles(World world, BlockPos pos, PlayerEntity player, PotionRackBlockEntity blockEntity, ItemStack stack, int slot){
+        if (world.isClient) {
+            return;
+        }
+        player.incrementStat(Stats.USED.getOrCreateStat(stack.getItem()));
+        SoundEvent soundEvent = stack.isOf(Items.GLASS_BOTTLE) ? SoundEvents.BLOCK_CHISELED_BOOKSHELF_INSERT_ENCHANTED : SoundEvents.BLOCK_CHISELED_BOOKSHELF_INSERT;
+        blockEntity.setStack(slot, stack.split(1));
+        world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        if (player.isCreative()) {
+            stack.increment(1);
+        }
+        world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+    }
+
+    public static void tryRemovingGlassBottles(World world, BlockPos pos, PlayerEntity player, PotionRackBlockEntity blockEntity, int slot) {
+        if (world.isClient) {
+            return;
+        }
+        ItemStack itemStack = blockEntity.removeStack(slot, 1);
+        SoundEvent soundEvent = itemStack.isOf(Items.GLASS_BOTTLE) ? SoundEvents.BLOCK_CHISELED_BOOKSHELF_PICKUP_ENCHANTED : SoundEvents.BLOCK_CHISELED_BOOKSHELF_PICKUP;
+        world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        if (!player.getInventory().insertStack(itemStack)) {
+            player.dropItem(itemStack, false);
+        }
+        world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+    }
+    ///For water bottles
+    public static void tryAddingWaterBottles(World world, BlockPos pos, PlayerEntity player, PotionRackBlockEntity blockEntity, ItemStack stack, int slot){
+        if (world.isClient) {
+            return;
+        }
+        player.incrementStat(Stats.USED.getOrCreateStat(stack.getItem()));
+        SoundEvent soundEvent = stack.isOf(Items.GLASS_BOTTLE) ? SoundEvents.BLOCK_CHISELED_BOOKSHELF_INSERT_ENCHANTED : SoundEvents.BLOCK_CHISELED_BOOKSHELF_INSERT;
+        blockEntity.setStack(slot, stack.split(1));
+        world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        if (player.isCreative()) {
+            stack.increment(1);
+        }
+        world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+    }
+
+    public static void tryRemovingWaterBottles(World world, BlockPos pos, PlayerEntity player, PotionRackBlockEntity blockEntity, int slot) {
+        if (world.isClient) {
+            return;
+        }
+        ItemStack itemStack = blockEntity.removeStack(slot, 1);
+        SoundEvent soundEvent = itemStack.isOf(Items.GLASS_BOTTLE) ? SoundEvents.BLOCK_CHISELED_BOOKSHELF_PICKUP_ENCHANTED : SoundEvents.BLOCK_CHISELED_BOOKSHELF_PICKUP;
+        world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        if (!player.getInventory().insertStack(itemStack)) {
+            player.dropItem(itemStack, false);
+        }
+        world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+    }
+    ///For Honey bottles
+    public static void tryAddingHoneyBottles(World world, BlockPos pos, PlayerEntity player, PotionRackBlockEntity blockEntity, ItemStack stack, int slot){
+        if (world.isClient) {
+            return;
+        }
+        player.incrementStat(Stats.USED.getOrCreateStat(stack.getItem()));
+        SoundEvent soundEvent = stack.isOf(Items.HONEY_BOTTLE) ? SoundEvents.BLOCK_HONEY_BLOCK_PLACE : SoundEvents.BLOCK_HONEY_BLOCK_HIT;
+        blockEntity.setStack(slot, stack.split(1));
+        world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        if (player.isCreative()) {
+            stack.increment(1);
+        }
+        world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+    }
+
+    public static void tryRemovingHoneyBottles(World world, BlockPos pos, PlayerEntity player, PotionRackBlockEntity blockEntity, int slot) {
+        if (world.isClient) {
+            return;
+        }
+        ItemStack itemStack = blockEntity.removeStack(slot, 1);
+        SoundEvent soundEvent = itemStack.isOf(Items.HONEY_BOTTLE) ? SoundEvents.BLOCK_HONEY_BLOCK_PLACE : SoundEvents.BLOCK_HONEY_BLOCK_HIT;
+        world.playSound(null, pos, soundEvent, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        if (!player.getInventory().insertStack(itemStack)) {
+            player.dropItem(itemStack, false);
+        }
+        world.emitGameEvent(player, GameEvent.BLOCK_CHANGE, pos);
+    }
+
+
+    @Nullable
+    @Override
+    public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return new PotionRackBlockEntity(pos, state);
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(HorizontalFacingBlock.FACING);
+        SLOT_OCCUPIED_PROPERTIES.forEach(builder::add);
+    }
+
+    @Override
+    public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
+        PotionRackBlockEntity potionRackBlockEntity;
+        if (state.isOf(newState.getBlock())) {
+            return;
+        }
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof PotionRackBlockEntity && !(potionRackBlockEntity = (PotionRackBlockEntity)blockEntity).isEmpty()) {
+            for (int i = 0; i < 6; ++i) {
+                ItemStack itemStack = potionRackBlockEntity.getStack(i);
+                if (itemStack.isEmpty()) continue;
+                ItemScatterer.spawn(world, pos.getX(), pos.getY(), pos.getZ(), itemStack);
+            }
+            potionRackBlockEntity.clear();
+            world.updateComparators(pos, this);
+        }
+        super.onStateReplaced(state, world, pos, newState, moved);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(HorizontalFacingBlock.FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(HorizontalFacingBlock.FACING, rotation.rotate(state.get(HorizontalFacingBlock.FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(HorizontalFacingBlock.FACING)));
+    }
+
+    @Override
+    public boolean hasComparatorOutput(BlockState state) {
+        return true;
+    }
+
+    @Override
+    public int getComparatorOutput(BlockState state, World world, BlockPos pos) {
+        if (world.isClient()) {
+            return 0;
+        }
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof PotionRackBlockEntity potionRackBlockEntity){
+            return potionRackBlockEntity.getLastInteractedSlot() + 1;
+        }
+        return 0;
+    }
+}
--- src/main/java/net/qzimyion/cellulose/entity/BlockEntity/PotionRackBlockEntity.java
+++ src/main/java/net/qzimyion/cellulose/entity/BlockEntity/PotionRackBlockEntity.java
@@ -1,55 +1,156 @@
 package net.qzimyion.cellulose.entity.BlockEntity;
 
+import com.mojang.logging.LogUtils;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ChiseledBookshelfBlock;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.qzimyion.cellulose.blocks.custom_blocks.PotionRackBlock;
+import net.qzimyion.cellulose.entity.CelluloseEntities;
+import net.qzimyion.cellulose.util.CelluloseTags;
+import org.slf4j.Logger;
 
+import java.util.Objects;
+import java.util.function.Predicate;
+
+@SuppressWarnings("unused")
 public class PotionRackBlockEntity extends BlockEntity implements Inventory {
-    public PotionRackBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
-        super(type, pos, state);
+    public static final int MAX_POTIONS = 6;
+    public static final int MAX_GLASS_BOTTLES = 6;
+    public static final int MAX_WATER_BOTTLES = 6;
+    public static final int MAX_HONEY_BOTTLES = 6;
+    private static final Logger LOGGER = LogUtils.getLogger();
+    private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(6, ItemStack.EMPTY);
+    private int lastInteractedSlot = -1;
+    public PotionRackBlockEntity(BlockPos pos, BlockState state) {
+        super(CelluloseEntities.POTION_RACK_BLOCK_ENTITY, pos, state);
+    }
+
+    private void updateState(int interactedSlot){
+        if (interactedSlot < 0 || interactedSlot >= 6) {
+            LOGGER.error("Expected slot 0-5, got {}", interactedSlot);
+            return;
+        }
+        this.lastInteractedSlot = interactedSlot;
+        BlockState blockState = this.getCachedState();
+        for (int i = 0; i < PotionRackBlock.SLOT_OCCUPIED_PROPERTIES.size(); ++i){
+            boolean bl = !this.getStack(i).isEmpty();
+            BooleanProperty booleanProperty = ChiseledBookshelfBlock.SLOT_OCCUPIED_PROPERTIES.get(i);
+            blockState = blockState.with(booleanProperty, bl);
+        }
+        Objects.requireNonNull(this.world).setBlockState(this.pos, blockState, Block.NOTIFY_ALL);
+    }
+
+    @Override
+    public void readNbt(NbtCompound nbt) {
+        this.inventory.clear();
+        Inventories.readNbt(nbt, this.inventory);
+        this.lastInteractedSlot = nbt.getInt("last_interacted_slot");
+    }
+
+    @Override
+    protected void writeNbt(NbtCompound nbt) {
+        Inventories.writeNbt(nbt, this.inventory, true);
+        nbt.putInt("last_interacted_slot", this.lastInteractedSlot);
+    }
+
+    public int getOpenSlotCount() {
+        return (int)this.inventory.stream().filter(Predicate.not(ItemStack::isEmpty)).count();
     }
 
     @Override
     public int size() {
-        return 0;
+        return 6;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.inventory.stream().allMatch(ItemStack::isEmpty);
     }
 
     @Override
     public ItemStack getStack(int slot) {
-        return null;
+        return this.inventory.get(slot);
     }
 
     @Override
     public ItemStack removeStack(int slot, int amount) {
-        return null;
+        ItemStack itemStack = Objects.requireNonNullElse(this.inventory.get(slot), ItemStack.EMPTY);
+        this.inventory.set(slot, ItemStack.EMPTY);
+        if (!itemStack.isEmpty()) {
+            this.updateState(slot);
+        }
+        return itemStack;
     }
 
     @Override
     public ItemStack removeStack(int slot) {
-        return null;
+        return this.removeStack(slot, 1);
     }
 
     @Override
     public void setStack(int slot, ItemStack stack) {
-
+        if (stack.isIn(CelluloseTags.Items.POTION_RACK_POTIONS)) {
+            this.inventory.set(slot, stack);
+            this.updateState(slot);
+        }
+        if (stack.isIn(CelluloseTags.Items.POTION_RACK_BOTTLES)) {
+            this.inventory.set(slot, stack);
+            this.updateState(slot);
+        }
+        if (stack.isIn(CelluloseTags.Items.POTION_RACK_WATER_BOTTLES)) {
+            this.inventory.set(slot, stack);
+            this.updateState(slot);
+        }
+        if (stack.isIn(CelluloseTags.Items.POTION_RACK_HONEY_BOTTLES)) {
+            this.inventory.set(slot, stack);
+            this.updateState(slot);
+        }
     }
 
     @Override
     public boolean canPlayerUse(PlayerEntity player) {
-        return false;
+        return Inventory.canPlayerUse(this, player);
+    }
+
+    @Override
+    public boolean isValid(int slot, ItemStack stack) {
+        return stack.isIn(CelluloseTags.Items.POTION_RACK_POTIONS)
+                && stack.isIn(CelluloseTags.Items.POTION_RACK_BOTTLES)
+                && stack.isIn(CelluloseTags.Items.POTION_RACK_WATER_BOTTLES)
+                && stack.isIn(CelluloseTags.Items.POTION_RACK_HONEY_BOTTLES)
+                && this.getStack(slot).isEmpty();
+    }
+
+    @Override
+    public boolean canTransferTo(Inventory hopperInventory, int slot, ItemStack stack) {
+        return hopperInventory.containsAny((ItemStack itemStack2) -> {
+            if (itemStack2.isEmpty()) {
+                return true;
+            }
+            return ItemStack.canCombine(stack, itemStack2) && itemStack2.getCount() + stack.getCount() <= Math.min(itemStack2.getMaxCount(), hopperInventory.getMaxCountPerStack());
+        });
+    }
+
+    @Override
+    public int getMaxCountPerStack() {
+        return 1;
+    }
+
+    public int getLastInteractedSlot() {
+        return this.lastInteractedSlot;
     }
 
     @Override
     public void clear() {
-
+        this.inventory.clear();
     }
 }
