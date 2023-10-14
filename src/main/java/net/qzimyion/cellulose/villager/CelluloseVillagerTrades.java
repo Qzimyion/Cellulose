3f3cb58d36ce05d4380c5de831f4efce43f479ee
Qzimyion <samyakchauhan676@gmail.com>
2023-10-14 11:40:12 +0530

    Updated some trades

--- src/main/java/net/qzimyion/cellulose/villager/CelluloseVillagerTrades.java
+++ src/main/java/net/qzimyion/cellulose/villager/CelluloseVillagerTrades.java
@@ -4,17 +4,22 @@ import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.village.TradeOffer;
+import net.qzimyion.cellulose.blocks.CelluloseBlocks;
 
 public class CelluloseVillagerTrades {
 
-    @SuppressWarnings("unused")
     public static void registerTrades(){
-        TradeOfferHelper.registerVillagerOffers(CelluloseVillagers.CARPENTER, 1, factories -> {
-                    final boolean add = factories.add((((entity, random) -> new TradeOffer(
-                            new ItemStack(Items.EMERALD, 3),
-                            new ItemStack(Items.OAK_LOG, 8),
-                            6, 2, 0.02f
-                    ))));}
+        TradeOfferHelper.registerVillagerOffers(CelluloseVillagers.CARPENTER, 1, factories -> factories.add((((entity, random) -> new TradeOffer(
+                new ItemStack(Items.EMERALD, 3),
+                new ItemStack(Items.OAK_LOG, 8),
+                6, 2, 0.02f
+        ))))
+        );
+        TradeOfferHelper.registerVillagerOffers(CelluloseVillagers.CARPENTER, 1, factories -> factories.add((((entity, random) -> new TradeOffer(
+                new ItemStack(Items.OAK_LOG, 16),
+                new ItemStack(CelluloseBlocks.AZALEA_LOG, 16),
+                10, 5, 0.04f
+        ))))
         );
     }
 }
