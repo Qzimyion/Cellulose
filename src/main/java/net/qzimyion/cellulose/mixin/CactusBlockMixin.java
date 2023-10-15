eb70fbd1539c0d047449db608f6c927e78fe4519
Qzimyion <samyakchauhan676@gmail.com>
2023-10-15 13:22:29 +0530

    Optimized code

--- src/main/java/net/qzimyion/cellulose/mixin/CactusBlockMixin.java
+++ src/main/java/net/qzimyion/cellulose/mixin/CactusBlockMixin.java
@@ -1,9 +1,7 @@
 package net.qzimyion.cellulose.mixin;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.CactusBlock;
-import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldView;
 import net.qzimyion.cellulose.blocks.CelluloseBlocks;
@@ -19,7 +17,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class CactusBlockMixin {
     @Inject(at = @At("TAIL"), method = "canPlaceAt", cancellable = true)
     public void canPlaceAt(BlockState state, WorldView world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
-        BlockState blockState2 = world.getBlockState(pos.down());
-        cir.setReturnValue((blockState2.isOf(Blocks.CACTUS) || blockState2.isIn(BlockTags.SAND) || (blockState2.isOf(CelluloseBlocks.STRIPPED_CACTUS))) && !world.getBlockState(pos.up()).isLiquid());
+        if(world.getBlockState(pos.down()).isOf(CelluloseBlocks.STRIPPED_CACTUS) && !world.getBlockState(pos.up()).isLiquid()) cir.setReturnValue(true);
     }
 }
