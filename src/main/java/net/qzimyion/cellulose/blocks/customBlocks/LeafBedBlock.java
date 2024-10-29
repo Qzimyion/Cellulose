fc6512a5e170e51d4af1850012e8d19b8f0d3c91
Qzimyion <samyakchauhan676@gmail.com>
2024-10-29 14:49:24 +0530

    Revisiting the leaf block concept

--- src/main/java/net/qzimyion/cellulose/blocks/customBlocks/LeafBedBlock.java
+++ src/main/java/net/qzimyion/cellulose/blocks/customBlocks/LeafBedBlock.java
@@ -1,26 +1,40 @@
 package net.qzimyion.cellulose.blocks.customBlocks;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.PlantBlock;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.BlockMirror;
 import net.minecraft.util.BlockRotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.World;
 
 @SuppressWarnings("deprecation")
 public class LeafBedBlock extends PlantBlock {
 
+    private static final float[] COLLISIONS = new float[]{1, 0.999f, 0.998f, 0.997f, 0.996f, 0.994f, 0.993f, 0.992f};
+    public static final IntProperty LAYERS = Properties.LAYERS;
     public static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
-    protected static final VoxelShape SHAPE = Block.createCuboidShape(1.0, 0.0, 1.0, 15.0, 1.5, 15.0);
+    protected static final VoxelShape[] SHAPE = new VoxelShape[]{VoxelShapes.empty(),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 2.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 10.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 12.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 14.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 16.0, 16.0)};
+
 
     public LeafBedBlock(Settings settings) {
         super(settings);
@@ -28,8 +42,26 @@ public class LeafBedBlock extends PlantBlock {
     }
 
     @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        if (state.get(LAYERS) >= 6){
+            return SHAPE[state.get(LAYERS)];
+        }
+        return VoxelShapes.empty();
+    }
+
+    @Override
+    public void onEntityCollision(BlockState state, World world, BlockPos pos, Entity entity) {
+        if (entity instanceof LivingEntity livingEntity){
+            if (state.get(LAYERS) >= 2){
+                float stuck = COLLISIONS[Math.max(0, state.get(LAYERS) - 1)];
+                livingEntity.slowMovement(state, new Vec3d(stuck, 1, stuck));
+            }
+        }
+    }
+
+    @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
-        return SHAPE;
+        return SHAPE[state.get(LAYERS)];
     }
 
     @Override
