976669f89dbddec45474ccb2fd8e3132476cee79
Qzimyion <samyakchauhan676@gmail.com>
2024-11-12 02:10:19 +0530

    Added some new features

--- src/main/java/net/qzimyion/cellulose/worldgen/CelluloseConfiguredFeatures.java
+++ src/main/java/net/qzimyion/cellulose/worldgen/CelluloseConfiguredFeatures.java
@@ -11,27 +11,46 @@ import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.AcaciaFoliagePlacer;
+import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.foliage.PineFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 import net.qzimyion.cellulose.Cellulose;
 import net.qzimyion.cellulose.blocks.CelluloseBlocks;
+import net.qzimyion.cellulose.worldgen.trees.foliagePlacers.CPineFoliagePlacer;
+import net.qzimyion.cellulose.worldgen.trees.treeDecorators.MossCarpetDecorator;
+import net.qzimyion.cellulose.worldgen.trees.trunkPlacers.HorizontalStraightTrunkPlacer;
 import net.qzimyion.cellulose.worldgen.trees.trunkPlacers.SmallDarkOakTrunkPlacer;
 
 import java.util.OptionalInt;
 
-import static net.minecraft.block.Blocks.DARK_OAK_LEAVES;
-import static net.minecraft.block.Blocks.DARK_OAK_LOG;
+import static net.minecraft.block.Blocks.*;
 
 public class CelluloseConfiguredFeatures {
 
+    //Fallen logs
+    public static final RegistryKey<ConfiguredFeature<?, ?>> FALLEN_OAK_LOG = registerKey("fallen_oak_log");
+    public static final RegistryKey<ConfiguredFeature<?, ?>> FALLEN_HOLLOW_OAK_LOG = registerKey("fallen_hollow_oak_log");
+
+    //Trees
     public static final RegistryKey<ConfiguredFeature<?, ?>> SMALL_DARK_OAK = registerKey("small_dark_oak");
     public static final RegistryKey<ConfiguredFeature<?, ?>> PINE_TREE = registerKey("pine_tree");
     public static final RegistryKey<ConfiguredFeature<?, ?>> PINE_TREE_B = registerKey("pine_tree_b");
     public static final RegistryKey<ConfiguredFeature<?, ?>> BIG_PINE_TREE = registerKey("big_pine_tree");
-
     public static void boostrap(Registerable<ConfiguredFeature<?, ?>> context){
 
+        //Fallen logs
+        register(context, FALLEN_OAK_LOG, Feature.TREE, new TreeFeatureConfig.Builder(BlockStateProvider.of(OAK_LOG),
+                new HorizontalStraightTrunkPlacer(3, 1, 2, 0.8F, 0.5F),BlockStateProvider.of(OAK_LEAVES),
+                new BlobFoliagePlacer(ConstantIntProvider.create(0), ConstantIntProvider.create(0), 0),
+                new TwoLayersFeatureSize(1, 0, 1)).ignoreVines().build());
+
+        register(context, FALLEN_HOLLOW_OAK_LOG, Feature.TREE, new TreeFeatureConfig.Builder(BlockStateProvider.of(CelluloseBlocks.HOLLOW_OAK_LOG),
+                new HorizontalStraightTrunkPlacer(3, 1, 2, 0.8F, 0.5F),BlockStateProvider.of(OAK_LEAVES),
+                new BlobFoliagePlacer(ConstantIntProvider.create(0), ConstantIntProvider.create(0), 0),
+                new TwoLayersFeatureSize(1, 0, 1)).ignoreVines().build());
+
+        //Trees
         register(context, SMALL_DARK_OAK, Feature.TREE, new TreeFeatureConfig.Builder(BlockStateProvider.of(DARK_OAK_LOG),
                 new SmallDarkOakTrunkPlacer(4, 1, 2, UniformIntProvider.create(4, 6), .95F,
                         UniformIntProvider.create(4, 6), Registries.BLOCK.getOrCreateEntryList(BlockTags.MANGROVE_LOGS_CAN_GROW_THROUGH)),
@@ -44,7 +63,7 @@ public class CelluloseConfiguredFeatures {
         register(context, PINE_TREE, Feature.TREE, new TreeFeatureConfig.Builder(BlockStateProvider.of(CelluloseBlocks.PINE_LOG),
                 new StraightTrunkPlacer(10, 1, 2),
                 BlockStateProvider.of(CelluloseBlocks.PINE_LEAVES),
-                new PineFoliagePlacer(UniformIntProvider.create(2, 2), UniformIntProvider.create(2, 3), UniformIntProvider.create(2, 12)),
+                new CPineFoliagePlacer(UniformIntProvider.create(2, 2), UniformIntProvider.create(2, 3), UniformIntProvider.create(2, 12)),
                 new TwoLayersFeatureSize(1, 0, 1)
         ).ignoreVines().build());
     }
--- src/main/java/net/qzimyion/cellulose/worldgen/CellulosePlacedFeatures.java
+++ src/main/java/net/qzimyion/cellulose/worldgen/CellulosePlacedFeatures.java
@@ -13,34 +13,48 @@ import net.qzimyion.cellulose.blocks.CelluloseBlocks;
 
 import java.util.List;
 
+import static net.minecraft.world.Heightmap.Type.MOTION_BLOCKING_NO_LEAVES;
+
 public class CellulosePlacedFeatures {
 
+    //Trees
     public static final RegistryKey<PlacedFeature> DARK_OAK_ROOFED_PLACED = registerKey("dark_oak_roofed");
     public static final RegistryKey<PlacedFeature> PINE_TREE_PLACED = registerKey("pine_tree");
+
+    //Fallen logs
+    public static final RegistryKey<PlacedFeature> FALLEN_OAK_LOG_PLACED = registerKey("fallen_oak_log");
 
 
     public static void boostrap(Registerable<PlacedFeature> context) {
         var configuredFeatureRegistryEntryLookup = context.getRegistryLookup(RegistryKeys.CONFIGURED_FEATURE);
 
-        register(context, DARK_OAK_ROOFED_PLACED,
-                configuredFeatureRegistryEntryLookup.getOrThrow(CelluloseConfiguredFeatures.SMALL_DARK_OAK),
-                VegetationPlacedFeatures.
-                        treeModifiersWithWouldSurvive
-                                (PlacedFeatures.createCountExtraModifier(2, 0.5f, 2),
-                                        Blocks.DARK_OAK_SAPLING));
+        //Trees
+        registerKey(context, DARK_OAK_ROOFED_PLACED, configuredFeatureRegistryEntryLookup.getOrThrow(CelluloseConfiguredFeatures.SMALL_DARK_OAK),
+                VegetationPlacedFeatures.treeModifiersWithWouldSurvive(PlacedFeatures.createCountExtraModifier(2, 0.5f, 2),
+                        Blocks.DARK_OAK_SAPLING));
 
-        register(context, PINE_TREE_PLACED, configuredFeatureRegistryEntryLookup.getOrThrow(CelluloseConfiguredFeatures.PINE_TREE),
+        registerKey(context, PINE_TREE_PLACED, configuredFeatureRegistryEntryLookup.getOrThrow(CelluloseConfiguredFeatures.PINE_TREE),
                 VegetationPlacedFeatures.treeModifiersWithWouldSurvive(PlacedFeatures.createCountExtraModifier(5, 0.1f, 1),
                         CelluloseBlocks.PINE_SAPLING));
+
+        //Fallen logs
+        registerKey(context, FALLEN_OAK_LOG_PLACED, configuredFeatureRegistryEntryLookup.getOrThrow(CelluloseConfiguredFeatures.FALLEN_OAK_LOG),
+                HeightmapPlacementModifier.of(MOTION_BLOCKING_NO_LEAVES),
+                RarityFilterPlacementModifier.of(3)
+        );
     }
 
     public static RegistryKey<PlacedFeature> registerKey(String name) {
-        return RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(Cellulose.MOD_ID, name));
+        return RegistryKey.of(RegistryKeys.PLACED_FEATURE, Identifier.of(Cellulose.MOD_ID, name));
     }
 
-    private static void register(Registerable<PlacedFeature> context, RegistryKey<PlacedFeature> key,
-                                 RegistryEntry<ConfiguredFeature<?, ?>> configuration,
-                                 List<PlacementModifier> modifiers) {
+    private static void registerKey(Registerable<PlacedFeature> context, RegistryKey<PlacedFeature> key, RegistryEntry<ConfiguredFeature<?, ?>> configuration,
+                                    List<PlacementModifier> modifiers) {
         context.register(key, new PlacedFeature(configuration, List.copyOf(modifiers)));
+    }
+
+    private static <FC extends FeatureConfig, F extends Feature<FC>> void registerKey(Registerable<PlacedFeature> context, RegistryKey<PlacedFeature> key,
+                                                                                      RegistryEntry<ConfiguredFeature<?, ?>> configuration, PlacementModifier... modifiers) {
+        registerKey(context, key, configuration, List.of(modifiers));
     }
 }
