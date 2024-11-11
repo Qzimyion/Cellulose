ec23b63ddad7cbb82dbe0c48e8c3c298995c2fee
Qzimyion <samyakchauhan676@gmail.com>
2024-11-12 02:07:02 +0530

    Added ShelfShroomBlock (WIP)

---
+++ src/main/java/net/qzimyion/cellulose/blocks/customBlocks/ShelfShroomBlock.java
@@ -0,0 +1,10 @@
+package net.qzimyion.cellulose.blocks.customBlocks;
+
+import net.minecraft.block.HorizontalFacingBlock;
+
+public class ShelfShroomBlock extends HorizontalFacingBlock {
+
+    protected ShelfShroomBlock(Settings settings) {
+        super(settings);
+    }
+}
