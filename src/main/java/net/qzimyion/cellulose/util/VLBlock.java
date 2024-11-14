11f11b5e882a7bcb0507b5ae622cf693b63196f2
Qzimyion <samyakchauhan676@gmail.com>
2024-11-14 16:47:31 +0530

    Added an item for victorian lilies and updated the Enum and block a bit.

--- src/main/java/net/qzimyion/cellulose/util/VLBlock.java
+++ src/main/java/net/qzimyion/cellulose/util/VLBlock.java
@@ -1,15 +1,15 @@
 package net.qzimyion.cellulose.util;
 
 import net.minecraft.util.StringIdentifiable;
-import net.minecraft.util.math.Direction;
 
 public enum VLBlock implements StringIdentifiable {
-    X("x_axis"),
-    Z("z_axis");
+    N_EAST("n_east"),
+    N_WEST("n_west"),
+    S_EAST("s_east"),
+    S_WEST("s_west");
 
-    public final String name;
-
-    VLBlock(String name){
+    private final String name;
+    VLBlock(String name) {
         this.name = name;
     }
 
@@ -20,9 +20,5 @@ public enum VLBlock implements StringIdentifiable {
     @Override
     public String asString() {
         return this.name;
-    }
-
-    public static VLBlock getInstance(Direction.Axis axis){
-        return axis == Direction.Axis.X ? VLBlock.X : VLBlock.Z;
     }
 }
