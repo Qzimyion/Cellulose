2bab52143c6d40ad610c9ff9f39c2b1d8c271643
Qzimyion <samyakchauhan676@gmail.com>
2024-11-21 00:40:23 +0530

    Updated the trunkplacer stuff

--- src/main/java/net/qzimyion/cellulose/worldgen/trees/trunkPlacers/HorizontalStraightTrunkPlacer.java
+++ src/main/java/net/qzimyion/cellulose/worldgen/trees/trunkPlacers/HorizontalStraightTrunkPlacer.java
@@ -5,15 +5,19 @@ import com.mojang.datafixers.Products;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.PillarBlock;
-import net.minecraft.registry.tag.FluidTags;
+import net.minecraft.block.Blocks;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.Heightmap;
 import net.minecraft.world.TestableWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.TreeFeature;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.foliage.FoliagePlacer;
+import net.minecraft.world.gen.placementmodifier.HeightmapPlacementModifier;
 import net.minecraft.world.gen.trunk.TrunkPlacer;
 import net.minecraft.world.gen.trunk.TrunkPlacerType;
 import net.qzimyion.cellulose.worldgen.WorldGenRegistry;
@@ -32,18 +36,16 @@ public class HorizontalStraightTrunkPlacer extends TrunkPlacer {
             horizontalCodec(instance).apply(instance, HorizontalStraightTrunkPlacer::new));
 
     @Contract("_ -> new")
-    protected static <P extends HorizontalStraightTrunkPlacer> Products.P5<RecordCodecBuilder.Mu<P>, Integer, Integer, Integer, Float, Float> horizontalCodec(RecordCodecBuilder.Instance<P> builder) {
-        return fillTrunkPlacerFields(builder)
-                .and(Codec.floatRange(0.0F, 1.0F).fieldOf("success_in_water_chance").forGetter((trunkPlacer) -> trunkPlacer.successInWaterChance))
-                .and(Codec.floatRange(0.0F, 1.0F).fieldOf("stump_chance").forGetter((trunkPlacer) -> trunkPlacer.stumpChance));
+    protected static <P extends HorizontalStraightTrunkPlacer> Products.P4<RecordCodecBuilder.Mu<P>, Integer, Integer, Integer, Float> horizontalCodec(RecordCodecBuilder.Instance<P> builder) {
+        return fillTrunkPlacerFields(builder).and(Codec.floatRange(0.0F, 1.0F).fieldOf("stump_chance").forGetter((trunkPlacer) -> trunkPlacer.stumpChance));
     }
 
-    public final float successInWaterChance;
     public final float stumpChance;
+    //public final float stumpVineChance;
+    //public final float stumpShroomChance;
 
-    public HorizontalStraightTrunkPlacer(int baseLength, int firstRandomHeight, int secondRandomHeight, float successInWaterChance, float stumpChance) {
+    public HorizontalStraightTrunkPlacer(int baseLength, int firstRandomHeight, int secondRandomHeight, float stumpChance) {
         super(baseLength, firstRandomHeight, secondRandomHeight);
-        this.successInWaterChance = successInWaterChance;
         this.stumpChance = stumpChance;
     }
 
@@ -52,8 +54,8 @@ public class HorizontalStraightTrunkPlacer extends TrunkPlacer {
         return WorldGenRegistry.HORIZONTAL_STRAIGHT_TRUNK_PLACER;
     }
 
-    public static boolean isWater(TestableWorld world, BlockPos pos) {
-        return world.testFluidState(pos, fluidstate -> fluidstate.isIn(FluidTags.WATER));
+    private boolean isInvalidBelowBlock(BlockState state) {
+        return state.isAir() || state.isIn(BlockTags.LEAVES);
     }
 
     public static int getRandomFromList(List<Integer> list) {
@@ -68,44 +70,71 @@ public class HorizontalStraightTrunkPlacer extends TrunkPlacer {
     @Override
     public List<FoliagePlacer.TreeNode> generate(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, int length, BlockPos startPos, TreeFeatureConfig config) {
         Direction.Axis logDir = random.nextBoolean() ? Direction.Axis.X : Direction.Axis.Z;
-        Direction direction = Direction.Type.HORIZONTAL.random(random);
-        //BlockState state = config.trunkProvider.get(random, startPos);
         List<FoliagePlacer.TreeNode> list = Lists.newArrayList();
-        if (isWater(world, startPos) && random.nextFloat() > this.successInWaterChance) {
-            return list;
-        }
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
         for (int i = 0; i < length; i++) {
-            BlockPos currentPos = startPos.offset(logDir, i);
-//            if (state.contains(Properties.AXIS) && logDir == Direction.Axis.X){
-//                state = state.with(AXIS, Direction.Axis.X);
-//            }else{
-//                state = state.with(AXIS, Direction.Axis.Z);
-//            }
             Function<BlockState, BlockState> blockStateBlockStateFunction = (state) -> {
-                if (state.contains(Properties.AXIS) && logDir == Direction.Axis.X){
+                if (state.contains(AXIS) && logDir == Direction.Axis.X){
                     state = state.with(AXIS, Direction.Axis.X);
                 }else{
                     state = state.with(AXIS, Direction.Axis.Z);
                 }
                 return state;
             };
+            int aboveSolidAmount = 0;
+            boolean isEndAboveSolid = false;
+            BlockPos currentPos = startPos.offset(logDir, i);
+            BlockPos newPos = world.getTopPosition(Heightmap.Type.OCEAN_FLOOR_WG, currentPos);
             if (random.nextFloat() < this.stumpChance){
                 if (i == length - 1) {
+                    BlockPos stumpPos;
                     List<Integer> distanceList = List.of(2, 3);
                     int placementDistance = getRandomFromList(distanceList);
-                    BlockPos stumpPos;
-                    //BlockState stumpState = config.trunkProvider.get(random, startPos).with(Properties.AXIS, Direction.Axis.Y);
-                    Function<BlockState, BlockState> stumpState = (state) -> state.with(Properties.AXIS, Direction.Axis.Y);
+                    Function<BlockState, BlockState> stumpState = (state) -> state.with(AXIS, Direction.Axis.Y);
                     if (logDir == Direction.Axis.X) {
-                        stumpPos = currentPos.offset(Direction.Axis.X, placementDistance);
+                        stumpPos = newPos.offset(Direction.Axis.X, placementDistance);
                     } else {
-                        stumpPos = currentPos.offset(Direction.Axis.Z, placementDistance);
+                        stumpPos = newPos.offset(Direction.Axis.Z, placementDistance);
+                    }
+                    BlockPos[] directions = new BlockPos[]{
+                            stumpPos.north(),
+                            stumpPos.south(),
+                            stumpPos.east(),
+                            stumpPos.west()
+                    };
+                    for (BlockPos pos : directions) {
+                        BlockState state = config.trunkProvider.get(random, pos);
+                        if (state.isAir()) {
+                            BlockState mossState = Blocks.VINE.getDefaultState();
+                            replacer.accept(pos, mossState);
+                        }
+                    }
+                    Iterable<BlockPos> poses = BlockPos.iterate(newPos, stumpPos);
+                    for (BlockPos blockPos : poses){
+                        mutable.add(blockPos);
+                        if (TreeFeature.canReplace(world, mutable)){
+                            if (!TreeFeature.canReplace(world, mutable.offset(Direction.DOWN)) && !TreeFeature.isAirOrLeaves(world, mutable)){
+                                aboveSolidAmount += 1;
+                                mutable.offset(Direction.UP);
+                                if (mutable.equals(newPos) || mutable.equals(stumpPos)){
+                                    isEndAboveSolid = true;
+                                }
+                            } else {
+                                mutable.offset(Direction.UP);
+                                if (mutable.equals(newPos)){
+                                    return list;
+                                }
+                            }
+                        } else {
+                            return list;
+                        }
                     }
                     getAndSetState(world, replacer, random, stumpPos, config, stumpState);
                 }
             }
-            startPos.add(currentPos);
-            getAndSetState(world, replacer, random, currentPos, config, blockStateBlockStateFunction);
+            if (isEndAboveSolid || ((double) aboveSolidAmount / (double) length) > 0.5){
+                getAndSetState(world, replacer, random, newPos, config, blockStateBlockStateFunction);
+            }
         }
         return list;
     }
